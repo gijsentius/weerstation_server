c0f0c0c44598244461b6d896834af068f22e6968
Gijs Entius <g.m.entius@gmail.com>
2017-12-19 21:08:08 +0100

    solved problems with opening a file

--- src/loggers/ExceptionLogger.java
+++ src/loggers/ExceptionLogger.java
@@ -1,5 +1,6 @@
 package loggers;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -7,7 +8,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * source: https://stackoverflow.com/questions/5175728/how-to-get-the-current-date-time-in-java
@@ -16,16 +20,57 @@ import java.time.LocalDateTime;
  * writes an messages to a log file
  */
 public class ExceptionLogger {
+    private static StringWriter sw;
+    private static int buffer;  // buffer is just an integer counting towards a certain number
+    private static int max_buffer;
+
+    public ExceptionLogger(int max_buffer_size) {
+        sw = new StringWriter();
+        buffer = 0;
+        max_buffer = max_buffer_size;
+    }
+
+    /**
+     * This method can be considered slow because it opens and closes files constantly
+     * @param e
+     */
     public synchronized static void logException(Exception e) {
-        Path file = Paths.get("error_log_" + LocalDateTime.now().toString() + ".txt");  // time is unique
-        // get stacktrace from exception
+//        String pathname = "C:\\Users\\Gijs\\Desktop\\TODO\\" + "error_log_" + LocalDate.now().toString() + ".txt";
+        String pathname = "error_log_" + LocalDate.now().toString() + ".txt";
+        Path file = Paths.get(pathname);
         StringWriter sw = new StringWriter();
-        e.printStackTrace(new PrintWriter(sw));
-        byte[] data = sw.toString().getBytes();
+        sw.append(LocalDateTime.now().toString() + "\r\n");
+        e.printStackTrace(new PrintWriter(sw)); // get stacktrace from exception
+        String data = sw.toString();
+        List<String> lines = Arrays.asList(data);
         try {
-            Files.write(file, data, StandardOpenOption.APPEND);  // Appends data to the file if it already exists
+            if (Files.exists(file)) {
+                Files.write(file, lines, StandardOpenOption.APPEND);
+            } else {
+                Files.write(file, lines, StandardOpenOption.CREATE_NEW);
+            }
         } catch (IOException ex) {
-            ExceptionLogger.logException(ex);  // TODO: check if this shows the stacktrace in the folder
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * More efficient method of writing to a file
+     * Build with help of the following web content
+     * source: https://stackoverflow.com/questions/1625234/how-to-append-text-to-an-existing-file-in-java
+     * source: https://stackoverflow.com/questions/1053467/how-do-i-save-a-string-to-a-text-file-using-java
+     */
+    public static synchronized void logExceptionsBuffered(Exception e) {
+        String pathname = "error_log_" + LocalDate.now().toString() + ".txt";
+        if (buffer > max_buffer) {
+            try(  PrintWriter out = new PrintWriter( pathname )  ) {
+                out.println(sw.toString());
+                sw = new StringWriter();
+                buffer = 0;
+            } catch (FileNotFoundException e1) {
+                e1.printStackTrace(new PrintWriter(sw));
+                buffer++;
+            }
         }
     }
 }
