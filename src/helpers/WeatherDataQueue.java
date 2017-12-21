3c7dda44eef97d08bbdfb97b1b065ef96e655619
Daggh <d.g.hoogendorp@st.hanze.nl>
2017-12-21 16:54:46 +0100

    DataIntegrity added calcTemp and checkData

--- src/helpers/WeatherDataQueue.java
+++ src/helpers/WeatherDataQueue.java
@@ -1,4 +1,26 @@
 package helpers;
 
+/*
+* This class will make a sliding window that is saved on the Pi.
+* The window will contain 30 measurements of a station
+* This will help check when there is a measurement
+* 30 * 800 queues = 24000 measurements
+ */
+
+import models.DataFrame;
+import models.DataFrameBuffer;
+
 public class WeatherDataQueue {
+
+    private int queueLength;
+    private DataFrameBuffer queuebuffer;
+
+    public WeatherDataQueue(int queueLength){
+        this.queueLength = queueLength;
+        queuebuffer = new DataFrameBuffer(queueLength);
+    }
+
+
+
+
 }
