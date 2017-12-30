dfffd8f20ebe6a42b7ff2ab1f44456ccaca62ca3
Gijs Entius <g.m.entius@gmail.com>
2017-12-30 12:12:33 +0100

    cleaned up weatherDatabase class

--- src/helpers/WeatherDatabase.java
+++ src/helpers/WeatherDatabase.java
@@ -39,30 +39,30 @@ public class WeatherDatabase extends DBConnector implements StorageHandler{
         StringBuilder query = new StringBuilder("INSERT INTO `measurements` ");
         StringBuilder query_item_names = new StringBuilder("(");
         StringBuilder query_item_values = new StringBuilder("(");
-            for (DataItem item : data) {
-                int counter = 0;
-                for (Object o : item.getData().entrySet()) {
-                    Map.Entry pair = (Map.Entry) o;
-                    if (counter == 0) {
-                        query_item_names.append("`" + pair.getKey() + "`");
-                        query_item_values.append("'" + pair.getValue() + "'");
-                        counter++;  // increment counter so this part will only run once
-                    } else {
-                        query_item_names.append(", " + '`' + pair.getKey() + '`');
-                        query_item_values.append(", " + '\'' + pair.getValue() + '\'');
-                    }
+        for (DataItem item : data) {
+            int counter = 0;
+            for (Object o : item.getData().entrySet()) {
+                Map.Entry pair = (Map.Entry) o;
+                if (counter == 0) {
+                    query_item_names.append("`" + pair.getKey() + "`");
+                    query_item_values.append("'" + pair.getValue() + "'");
+                    counter++;  // increment counter so this part will only run once
+                } else {
+                    query_item_names.append(", " + '`' + pair.getKey() + '`');
+                    query_item_values.append(", " + '\'' + pair.getValue() + '\'');
                 }
-                query_item_names.append(")");
-                query_item_values.append(")");
-                query.append(query_item_names);
-                query.append(" VALUES ");
-                query.append(query_item_values);
+            }
+            query_item_names.append(")");
+            query_item_values.append(")");
+            query.append(query_item_names);
+            query.append(" VALUES ");
+            query.append(query_item_values);
 //                System.out.println(query.toString());
 //            ResultSet rs = statement.executeQuery(query.toString()); // just an example
 //            rs.close();
-            }
         }
-            /*
-            INSERT INTO `measurements` (`STN`, `TIMESTAMP`, `TEMP`, `DEWP`, `STP`, `SLP`, `VISIB`, `WDSP`, `PRCP`, `SNDP`, `FRSHTT`, `CLDC`, `WNDDIR`) VALUES ('3456', CURRENT_TIMESTAMP, '456', '4365', '456', '3465', '4365', '3465', '3465', '3465', '3465', '345', '3465')
-             */
+    }
 }
+/*
+INSERT INTO `measurements` (`STN`, `TIMESTAMP`, `TEMP`, `DEWP`, `STP`, `SLP`, `VISIB`, `WDSP`, `PRCP`, `SNDP`, `FRSHTT`, `CLDC`, `WNDDIR`) VALUES ('3456', CURRENT_TIMESTAMP, '456', '4365', '456', '3465', '4365', '3465', '3465', '3465', '3465', '345', '3465')
+ */
