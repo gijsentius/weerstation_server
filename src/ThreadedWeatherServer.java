231f0b0ef3970a11fe231e38d7f863fbc4f2925e
Daggh <d.g.hoogendorp@st.hanze.nl>
2018-01-15 16:41:21 +0100

    poort 22

--- src/ThreadedWeatherServer.java
+++ src/ThreadedWeatherServer.java
@@ -52,7 +52,7 @@ public class ThreadedWeatherServer implements Runnable{
     {
         try {
             System.out.println("De server werkt");
-            new ThreadedWeatherServer(8080, 10, 800).run();
+            new ThreadedWeatherServer(22, 10, 800).run();
         } catch (IOException | SQLException e) {
 //                ts.terminate();  // maybe a more subtle solution can be found for the termination of the socket
                 ExceptionLogger.logException(e);
